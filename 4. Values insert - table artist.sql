94623cc800d505ad5f29974e2e955f4c9514c400
Ana Carolina Martins <48389153+carolinnmartins@users.noreply.github.com>
2022-07-06 10:54:39 +0200

    Add files via upload
    These 3 scripts have the values inserted into the tables.

---
+++ 4. Values insert - table artist.sql
@@ -0,0 +1,30 @@
+USE clm_wings_stadium;
+
+SELECT * FROM artist;
+
+INSERT INTO artist (artist_name, artist_income)
+VALUES ('50 Cent','250000'),
+('Alicia Keys','350000'),
+('Allisic','50000'),
+('Andrea Bocelli','550000'),
+('Billie Eilish','400000'),
+('Coldplay','750000'),
+('Ed Sheeran','340000'),
+('Elton John','700000'),
+('Evanescence ','215000'),
+('Foster The People','132000'),
+('Hans Zimmer','450000'),
+('Helene Fischer','200000'),
+('Iron Maiden','240000'),
+('Justin Bieber','650000'),
+('Kanye West','500000'),
+('Kiss','340000'),
+('Ozuna','125000'),
+('Paw Patrol','100000'),('Pearl Jam','110000'),
+('Queen & Adam Lambert','560000'),
+('Rage Against the Machine','110000'),
+('Swedish House Mafia','459000'),
+('The weeknd','500000'),
+('Within Temptation','320000');
+
+
